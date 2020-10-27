cfb4edfe4026c714c05271a73b225717658b8017
Do Ngoc Tuan <54639734+dongoctuanr2b@users.noreply.github.com>
2020-10-27 21:42:24 +0700

    fixed stack overflow: null

--- src/main/java/com/tuan/recipeapp/models/Notes.java
+++ src/main/java/com/tuan/recipeapp/models/Notes.java
@@ -1,10 +1,12 @@
 package com.tuan.recipeapp.models;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = "recipe")
 @Entity
 public class Notes {
 
